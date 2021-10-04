70fca798520f17073c9fe8bd523ed24fef0b6d51
harunrzl <github@harun.id>
2021-10-04 09:22:26 +0700

    chore: bump package version

--- packages/hooks/src/useFetch/utils/isEmpty.js
+++ packages/hooks/src/useFetch/utils/isEmpty.js
@@ -1,7 +1,7 @@
 // because Object.keys(new Date()).length === 0;
 // we have to do some additional check
-const isEmpty = obj => !obj || // 👈 null and undefined check
-  && Object.keys(obj).length === 0
+const isEmpty = obj => !obj // 👈 null and undefined check
+  || Object.keys(obj).length === 0
   && obj.constructor === Object;
 
 export default isEmpty;
